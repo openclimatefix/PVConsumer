f370506533c00360ebab39040476aa982fe0db53
peterdudfield <peter.dudfield@hotmail.com>
2022-02-18 11:39:02 +0000

    add option for DB_URL

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from nowcasting_datamodel.connection import Base_PV, DatabaseConnection
 @pytest.fixture
 def db_connection():
 
-    url = os.getenv("DB_URL_PV", "sqlite:///test.db")
+    url = os.getenv("DB_URL", "sqlite:///test.db")
 
     connection = DatabaseConnection(url=url, base=Base_PV, echo=False)
     Base_PV.metadata.create_all(connection.engine)
