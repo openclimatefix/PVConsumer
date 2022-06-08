79e433ca5b06dbd270e32eeab66a003b201d0616
peterdudfield <peter.dudfield@hotmail.com>
2022-06-08 10:41:18 +0100

    fix

--- pvconsumer/app.py
+++ pvconsumer/app.py
@@ -192,9 +192,10 @@ def pull_data_and_save(
                     if len(pv_yield_df) > 1:
                         if pv_yield_df.iloc[-1].solar_generation_kw == 0 \
                             and pv_yield_df.iloc[-2].solar_generation_kw != 0:
-                            logger.debug(f'Dropping last row of pv data for {pv_system.pv_system_id} '
+                            logger.debug(f'Dropping last row of pv data for '
+                                         f'{pv_system.pv_system_id} '
                                          f'as last row is 0, but the second to last row is not.')
-                            pv_yield_df.drop(df.tail(1).index, inplace=True)
+                            pv_yield_df.drop(pv_yield_df.tail(1).index, inplace=True)
 
                     pv_yield_df = pv_yield_df[pv_yield_df["datetime"] > last_pv_yield_datetime]
 
