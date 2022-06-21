8c8a74ad2565de9a4dd79f37a987286a2b1a3330
peterdudfield <peter.dudfield@hotmail.com>
2022-06-21 10:14:08 +0100

    lint

--- pvconsumer/solar_sheffield_passiv.py
+++ pvconsumer/solar_sheffield_passiv.py
@@ -95,7 +95,7 @@ def get_all_latest_pv_yield_from_solar_sheffield() -> pd.DataFrame:
     data_df.rename(columns={"timestamp": "datetime_utc"}, inplace=True)
 
     # change from Watts the KW
-    data_df.loc[:,"solar_generation_kw"] = data_df['data'] / 1000
+    data_df.loc[:, "solar_generation_kw"] = data_df['data'] / 1000
 
     # add timestamp UTC
     data_df['datetime_utc'] = data_df['datetime_utc'].dt.tz_localize('UTC')
