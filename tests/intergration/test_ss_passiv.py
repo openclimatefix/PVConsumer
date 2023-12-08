eab6642350540277525b499c93adec715e9b754c
peterdudfield <peter.dudfield@hotmail.com>
2023-12-08 13:13:48 +0000

    update test

--- tests/intergration/test_ss_passiv.py
+++ tests/intergration/test_ss_passiv.py
@@ -11,7 +11,7 @@ def test_get_all_systems():
 
     # these numbers seem to change over time
     assert len(pv_systems) >= 56824
-    assert len(pv_systems) <= 56970
+    assert len(pv_systems) <= 57200
     assert pv_systems[0].installed_capacity_kw is not None
 
 
